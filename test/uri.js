941739179b05cf6b8e9cfa75e683bc58732053df
Adrian Rossouw <adrian@daemon.co.za>
2014-07-20 06:29:06 +0200

    Start tests: turning objects stateful
    Any javascript object can now be turned into an implementation
    of the state machine we built.
    
    It will now have the this.state() accessor attached to it.

---
+++ test/uri.js
@@ -0,0 +1,22 @@
+/* global describe, it, before, beforeEach, after, afterEach */
+var should = require('should');
+
+var state = require('state');
+
+// Load the state machine definition
+var smUri = require('../uri');
+
+describe('URI state machine', function() {
+  // plain javascript object
+  var uri = {};
+
+  // wrap the object with a state machine
+  before(function() {
+    state(uri, smUri);
+  });
+
+  it('makes objects stateful', function() {
+    should.exist(uri.state);
+    uri.state.should.be.Function;
+  });
+});
